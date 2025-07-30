7836fb55ef79c48e88551c3ec72f992c9b74c2df
thangnv-0703 <sunshines0703@gmail.com>
2025-07-30 20:14:34 +0700

    remove old project

--- src/HiveSpace.IdentityService/HiveSpace.IdentityService.Application/Models/Responses/SignupResponseDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HiveSpace.IdentityService.Application.Models.Responses;
-
-public record SignupResponseDto(
-    string Email,
-    string FullName,
-    string UserName,
-    Guid UserId
-);
