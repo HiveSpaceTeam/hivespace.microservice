76b15d5c7a057d4e539797baa72f80eac91d1464
Nguyễn Phúc Tĩnh <phuctinh2001@gmail.com>
2025-08-02 17:32:44 +0700

    Catalog service

---
+++ src/HiveSpace.CatalogService/HiveSpace.CatalogService.Domain/Aggregates/SkuAggregate/SkuImage.cs
@@ -0,0 +1,22 @@
+﻿using HiveSpace.Domain.Shared.Entities;
+
+namespace HiveSpace.CatalogService.Domain.Aggregates.SkuAggregate
+{
+    public class SkuImage : ValueObject
+    {
+        public string SkuId { get; private set; }
+        public string FileId { get; private set; }
+
+        public SkuImage(string productId, string fileId)
+        {
+            SkuId = productId;
+            FileId = fileId;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return SkuId;
+            yield return FileId;
+        }
+    }
+}
