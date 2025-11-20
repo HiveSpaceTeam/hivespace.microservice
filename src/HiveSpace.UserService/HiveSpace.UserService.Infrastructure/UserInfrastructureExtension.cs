a75df0b920827c53957f7acdf72d856f01675f14
thangnv-0703 <sunshines0703@gmail.com>
2025-11-20 22:13:46 +0700

    Fix error handling in AddUserDbContext method for UserServiceDb connection string

--- src/HiveSpace.UserService/HiveSpace.UserService.Infrastructure/UserInfrastructureExtension.cs
+++ src/HiveSpace.UserService/HiveSpace.UserService.Infrastructure/UserInfrastructureExtension.cs
@@ -24,10 +24,11 @@ public static class UserInfrastructureExtension
     public static void AddUserDbContext(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("UserServiceDb");
+        System.Console.WriteLine("UserServiceDb Connection String: " + connectionString);
         if (string.IsNullOrWhiteSpace(connectionString))
         {
             var error = new Error(CommonErrorCode.ConfigurationMissing, "UserServiceDb");
-            throw new HiveSpace.Core.Exceptions.ApplicationException(new[] { error }, 500, false);
+            throw new HiveSpace.Core.Exceptions.ApplicationException([error], 500, false);
         }
 
         // Register interceptors manually
