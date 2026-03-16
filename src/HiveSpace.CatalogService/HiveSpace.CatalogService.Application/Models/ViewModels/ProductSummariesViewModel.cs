952b276d665ebb84c8fa6d243cd574235385abbe
Nguyễn Phúc Tĩnh <phuctinh2001@gmail.com>
2026-03-16 22:26:53 +0700

    Feature/masstransit messaging (#75)
    * Masstransit
    
    * Handle StoreCreatedEvent
    
    * Masstransit
    
    * Outbox
    
    * Outbox Pattern
    
    * remove migration
    
    * remove migrations
    
    * Refator code
    
    * refactor
    
    * Create an auto-deploy file
    
    * Unlink the containerApp ca-hivespace-user-svc-dev from this repo
    
    * remove folders
    
    * Remove unessesary files
    
    * remove unessary files
    
    * remove unessary files
    
    * remove migrations
    
    * update
    
    * update
    
    * update
    
    * Refactor
    
    * Refactor code
    
    * undo
    
    * update
    
    * refactor
    
    * remove
    
    * merge code
    
    * delete file
    
    * add IncomingRequest
    
    * undo
    
    * product
    
    * product
    
    * Product Detail
    
    * undo
    
    ---------
    
    Co-authored-by: thangnv-0703 <sunshines0703@gmail.com>

---
+++ src/HiveSpace.CatalogService/HiveSpace.CatalogService.Application/Models/ViewModels/ProductSummariesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiveSpace.CatalogService.Application.Models.ViewModels
+{
+    public class ProductSummariesViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
