bd3a95361859fcce5cf66762f69969559825d673
thangnv-0703 <sunshines0703@gmail.com>
2026-03-30 21:47:18 +0700

    feat: Update catalog database schema with decimal types for weight and dimensions (#80)

---
+++ src/HiveSpace.CatalogService/HiveSpace.CatalogService.Infrastructure/Migrations/20260330132103_UpdateCatalogDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HiveSpace.CatalogService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCatalogDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
